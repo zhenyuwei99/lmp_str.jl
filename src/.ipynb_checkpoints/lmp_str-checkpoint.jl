9393e88a84c9e61452bfbd3f53331132ac1e7b01
zhenyuwei99 <zhenyuwei99@gmail.com>
2020-04-21 20:43:25 +0800

    v-0.0.2

--- src/.ipynb_checkpoints/lmp_str-checkpoint.jl
+++ src/.ipynb_checkpoints/lmp_str-checkpoint.jl
@@ -1,17 +1,13 @@
 module lmp_str
 
 import Dates
+import Printf
 
 abstract type Str end
 abstract type Data end
 abstract type Unit end
 
 include("Constants.jl")
-export Const_k_b, Const_n_a, Const_density_wat,
-    Const_kcal2j, Const_kcalm2j, Const_kcalm2t,
-    Const_g2kg, Const_kg2g, Const_gm2g, Const_gm2kg,
-    Const_an2m, Const_an2nm, Const_nm2m, Const_cm2an, Const_dm2m, Const_m2dm,
-    Const_fs2s, Const_ps2s, Const_ns2s
 
 include("./Family/Family_Basic.jl")
 export Family_Basic, transform, SC, BCC, FCC, DC
@@ -24,6 +20,9 @@ export Family_Si, Si, Si3N4, Si3N4_Ort, SiO2
 
 include("./Family/Family_C.jl")
 export Family_C, Graphene, Graphene_Ort
+
+include("./Family/Family_Ti.jl")
+export Family_Ti, TiO2
 
 include("./Family/Family_Ion.jl")
 export Family_Ion
@@ -48,17 +47,6 @@ include("combine.jl")
 export cat_data, sort_data!
 
 include("write.jl")
-export write_data, write_info
-
-
-
-
-
-
-
-
-
-
-
+export write_data, write_info, write_xyz
 
 end # module
--- src/.ipynb_checkpoints/write-checkpoint.jl
+++ src/.ipynb_checkpoints/write-checkpoint.jl
@@ -2,14 +2,14 @@
 """
     write_data(data::Data, name_file::AbstractString)
 
-Do this will write all information in `data` to file `name_file`
+Do this will write all information in `data` to file `name_file` in `.data` formation
 
 # Example
 ```julia-repl
 data_cell = genr_cell([10 10 3])
 str = Si3N4()
 data_atom = genr_atom(data_cell, str)
-write(data_atom, "test.data")
+write_data(data_atom, "test.data")
 ```
 """
 function write_data(data::Data, name_file::AbstractString)
@@ -145,3 +145,30 @@ function write_info(info::Int64, name_file::AbstractString)
     # do nothing for vec_bond or vec_angle = 0
 end
 
+"""
+    write_xyz(data::Data, name_file::AbstractString)
+Do this will write all information in `data` to file `name_file` in `.xyz` formation
+
+# Example
+```julia-repl
+data_cell = genr_cell([10 10 3])
+str = TiO2()
+data_atom = genr_atom(data_cell, str)
+write_xyz(data_atom, "test.xyz")
+```
+"""
+function write_xyz(data::Data, name_file::AbstractString)
+    io = open(name_file, "w")
+    num_atoms = data.data_basic.num_atoms
+    name_atoms = data.data_str.atom_name
+    @printf(io, "%.d\n", num_atoms)
+    @printf(io, ".XYZ file of %s and %s created by lmp_str \n", name_atoms[1], name_atoms[2])
+    for atom = 1:num_atoms
+        @printf(io, "%s", name_atoms[data.vec_atom[atom].typ])
+        for dim = 1:3
+            @printf(io, "\t%4.4f", data.vec_atom[atom].coord[dim])
+        end
+        @printf(io, "\n")
+    end
+    close(io)
+end
