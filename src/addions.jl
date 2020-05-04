1eac855cb92d4b2304d36574d0249192ab763807
zhenyuwei99 <zhenyuwei@gmail.com>
2020-05-04 00:55:19 -0700

    v-0.0.2

--- src/addions.jl
+++ src/addions.jl
@@ -1,8 +1,10 @@
 # addions
 """
-    addions(data::Data, ion_type::String, conc::Float64)
+    function addions(data::Data, ion_type::String; num_pairs=false, conc=false)
 
-Do this will return a variable in "Data_Unit" type containing information of ions to make a solution with `conc`.
+This will return a variable in "Data_Unit" type containing information of ions. Two options are offered, determining by which parameters, `num_pairs` or `conc`, has been specified. 
+- `num_pairs` will create ion composite with the number of `num_ions`.
+- `conc` will create ion composite with the number which make the solution with a concentration of `conc`.
 
 # Supported List
 - Type          Mass(g/mol)
@@ -17,16 +19,24 @@ Notcice: `ion_type` is case sensetive and space is needed for recogonition
 cell = genr_cell([30,20,3])
 str = Tip3p()
 data = genr_atom(cell, str)
-data = addions(data, "Na Cl", 0.5)
+data = addions(data, "Na Cl", conc=0.5)
 ```
 """
-function addions(data::Data, ion_type::String, conc::Float64)
+function addions(data::Data, ion_type::String; num_pairs=false, conc=false)
     # Supported List
     list_ion = split("K Na Fe Al Cl")
     list_charge = [1, 1, 2, 3, -1]
     list_mass = [39.09, 22.99, 55.845, 26.98, 35.453]
-
+    
     # Reading Input
+    if num_pairs==false | conc==false
+        error("One of `num_pairs` or `conc` should be specified")
+    end
+    
+    if num_pairs!=false | conc!=false
+        error("Only one of `num_pairs` or `conc` can be specified")
+    end
+    
     ion = split(ion_type)
     len = length(ion)
     ion_id =  [findall(x->x==ion[n], list_ion)[1] for n = 1:len]
@@ -34,23 +44,26 @@ function addions(data::Data, ion_type::String, conc::Float64)
     ion_mass = [list_mass[ion_id[n]] for n = 1:len]
 
     # Genrate ion_mode
-    num_unit_ions = lcm(ion_charge[1], ion_charge[2])
-    num_ions = convert.(Int64, [abs(num_unit_ions/ion_charge[1]), abs(num_unit_ions/ion_charge[2])])
+    num_unit_ions = lcm(abs.(ion_charge))
+    num_ions = convert.(Int64, num_unit_ions ./ abs.(ion_charge))
     num_unit_ions = sum(num_ions)
-
-    unit_type = vcat([1 for i = 1:num_ions[1]], [2 for i = 1:num_ions[2]])
-    unit_charge = vcat([ion_charge[1] for i = 1:num_ions[1]], [ion_charge[2] for i = 1:num_ions[2]])
-
+    
+    unit_type = vcat([[j for i=1:num_ions[j]] for j=1:len]...) # Genrate type vec like [1 2 2 2]
+    unit_charge = vcat([[ion_charge[j] for i=1:num_ions[j]] for j=1:len]...)
+    
     center = (max(data.vec_atom, "coord")-min(data.vec_atom, "coord")) ./ 2
     ion_mode = [Atom(i, i, unit_type[i], unit_charge[i], center) for i = 1:num_unit_ions]
-
+    
     # Calculate # of solute molecules
-    ratio_sol2wat = conc / (Const_density_wat * Const_kg2g / Const_m2dm^3 / sum(data.data_str.atom_mass))
-
-    num_wat = data.data_basic.num_atoms
-    num_sol = convert(Int64, round(num_wat * ratio_sol2wat))
+    if conc!= false
+        ratio_sol2wat = conc / (Const_density_wat * Const_kg2g / Const_m2dm^3 / sum(data.data_str.atom_mass)) 
+        num_wat = data.data_basic.num_atoms
+        num_sol = convert(Int64, round(num_wat * ratio_sol2wat))
+    else
+        num_sol = num_pairs
+    end
     num_atoms = num_sol * num_unit_ions
-
+    
     # Generate new Atoms
     vec_atom = data.vec_atom
     vec_atom_new = Vector{Atom}(undef, num_atoms)
@@ -67,7 +80,7 @@ function addions(data::Data, ion_type::String, conc::Float64)
             vec_atom_new[id_now] = atom_now
         end
     end
-
+    
     # Generate new Data
     data_str = Family_Ion(ion, ion_charge, ion_mass, len, [n for n = 1:len])
     data_basic = Data_Basic(data.data_basic)
@@ -77,7 +90,7 @@ function addions(data::Data, ion_type::String, conc::Float64)
     data_basic.num_bond_types = 0
     data_basic.num_angles = 0
     data_basic.num_angle_types = 0
-
+    
     # Output
     data_ion = Data_Unit(data_basic, data_str, vec_atom_new, 0, 0)
 end
