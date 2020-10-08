3a3328fd0fa7a8a0b8a166b6fe4a09dd1e3bb214
ZhenyuWei <zhenyuwei99@gmail.com>
2020-10-08 21:04:01 +0800

    v-0.0.5

--- src/Constants.jl
+++ src/Constants.jl
@@ -1,4 +1,35 @@
+# Physical Constants
+Const_mu_0 = 4e-7 * π;
+Const_epsilong_0 = 8.854188e-12;
+Const_e = 1.6e-19;
+Const_k = 9e9;
+Const_k_b = 1.38065e-23;
+Const_n_a = 6.02214e23;
+Const_density_wat = 1e3;      # Unit: kg/m^3
+# Energy Converters
+Const_kcal2j = 4.184e3;
+Const_kcalm2j = Const_kcal2j/Const_n_a;
+Const_kcalm2t = Const_kcalm2j/Const_k_b;
+# Mass Converters
+Const_g2kg = 1e-3;
+Const_kg2g = 1e3;
+Const_gm2g = 1/Const_n_a;
+Const_gm2kg = Const_g2kg/Const_n_a;
+# Length Converters
+Const_an2m = 1e-10;
+Const_an2nm = 1e-1;
+Const_nm2m = 1e-9;
+Const_cm2an = 1e8;
+Const_dm2m = 1e-1;
+Const_m2dm = 1/Const_dm2m;
+# Time Converters
+Const_fs2s = 1e-15;
+Const_ps2s = 1e-12;
+Const_ns2s = 1e-9;
+
 """
+   function constants() 
+This will return a `Dict` which contains constants shown below:
 # Constants List:
 - Physical Constants
    - `mu_0`: Permeability of vacuum, Unit: N/A^2;
@@ -64,32 +95,3 @@ function constants()
 end
 
 const_list = constants()
-
-# Physical Constants
-Const_mu_0 = 4e-7 * π;
-Const_epsilong_0 = 8.854188e-12;
-Const_e = 1.6e-19;
-Const_k = 9e9;
-Const_k_b = 1.38065e-23;
-Const_n_a = 6.02214e23;
-Const_density_wat = 1e3;      # Unit: kg/m^3
-# Energy Converters
-Const_kcal2j = 4.184e3;
-Const_kcalm2j = Const_kcal2j/Const_n_a;
-Const_kcalm2t = Const_kcalm2j/Const_k_b;
-# Mass Converters
-Const_g2kg = 1e-3;
-Const_kg2g = 1e3;
-Const_gm2g = 1/Const_n_a;
-Const_gm2kg = Const_g2kg/Const_n_a;
-# Length Converters
-Const_an2m = 1e-10;
-Const_an2nm = 1e-1;
-Const_nm2m = 1e-9;
-Const_cm2an = 1e8;
-Const_dm2m = 1e-1;
-Const_m2dm = 1/Const_dm2m;
-# Time Converters
-Const_fs2s = 1e-15;
-Const_ps2s = 1e-12;
-Const_ns2s = 1e-9;
