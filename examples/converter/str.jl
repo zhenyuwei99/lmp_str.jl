da3b9e8a0ba464f8b1fb995f3300d3875aaa17cf
ZhenyuWei <zhenyuwei99@gmail.com>
2020-10-18 11:53:20 +0800

    add converter example

--- examples/converter/str.jl
+++ examples/converter/str.jl
@@ -1,8 +1,8 @@
 using lmp_str
 
+file_out = "test.data"
 file_pdb = "test.pdb"
 file_psf = "test.psf"
-file_out = "test.data"
 
 data = converter_vmd(file_pdb, file_psf, potential_charmm36)
 write_data(data, file_out)
